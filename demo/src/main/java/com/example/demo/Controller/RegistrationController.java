748babc81b60503edbcd74a20500c624db17902a
Alan Lands <43272925+Alanlands1@users.noreply.github.com>
2024-09-02 11:29:25 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Controller/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Model.MyAppUser;
+import com.example.demo.Model.MyAppUserRepository;
+
+@RestController
+public class RegistrationController {
+    
+    @Autowired
+    private MyAppUserRepository myAppUserRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @PostMapping(value = "/req/signup", consumes = "application/json")
+    public MyAppUser createUser(@RequestBody MyAppUser user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return myAppUserRepository.save(user);
+    }
+    
+}
